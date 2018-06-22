274dcd565d08cab62212dfd982dde2a297eaea25
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-22 12:24:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,21 @@ class Anagram
     @anagram = anagram
   end
 
-  
+  def match(array)
+    matches_array = []
+    newArray = array.split(",")
+    newArray.each do |word|
+      letter_array = word.split("")
+      if letter_array.sort == array
+        matches_array << word
+      else
+        matches_array
+      end
+    end      
+
+  end
+
+
 
 
 end
